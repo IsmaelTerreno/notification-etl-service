886e9ac1bf1280a3d332b2018129ae226e8f57ff
IsmaelTerreno <elismakof2002@gmail.com>
2024-10-03 17:56:17 -0300

    Add Swagger documentation setup
    Integrated Swagger to the project by installing @nestjs/swagger and configuring it in src/main.ts. This allows the API documentation to be accessible at the '/doc' endpoint, providing a more comprehensive way to interact with and understand the Notification service API.

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 /**
  * Gets the cors origin to respect the expressjs cors which is behind scenes in NestJs
@@ -32,6 +33,15 @@ async function bootstrap() {
     }),
   );
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('Notification service API')
+    .setDescription(
+      'Service to manage notifications related to existing users with a account to receive updates.',
+    )
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(SERVER_PORT_LISTENING);
 }
 
